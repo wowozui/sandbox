41079400a14ec96d27643f8c6c579497f2b2b1c6
wowozui <159249781+wowozui@users.noreply.github.com>
2025-02-03 09:12:25 +0100

    Create sandbox.js
    Add sandbox language mod".

---
+++ sandbox.js
@@ -0,0 +1,30 @@
+// Define the language code and name
+languageList.push("sandbox");
+languageNames.sandbox = "Sandbox Language";
+
+// Create translations object
+elements.names.sandbox = {
+    "water": "Water",
+    "fire": "Fire",
+    "earth": "Earth",
+    "air": "Air",
+    "sand": "Sand",
+    "stone": "Stone",
+    "lava": "Lava",
+    "steam": "Steam"
+};
+
+// Create category translations
+categories.names.sandbox = {
+    "powders": "Powders",
+    "liquids": "Liquids",
+    "gases": "Gases",
+    "solids": "Solids"
+};
+
+// Create interface translations
+strings.sandbox = {
+    "loading": "Loading...",
+    "pauseMessage": "Game Paused",
+    "menuMessage": "Main Menu"
+};
